deb7423f675f97479e00042996d7a3e9cdd1bb31
mfw-bala <kamandla.b@moneyforward.co.jp>
2025-02-11 19:59:19 +0900

    remove routes/ and use server.go

--- backend/main.go
+++ backend/main.go
@@ -1,41 +1,6 @@
 package main
 
-import (
-	"log"
-
-	"github.com/baala3/passkey-demo/webauthn"
-
-	"github.com/baala3/passkey-demo/routes"
-	"github.com/gin-contrib/sessions"
-	gormsessions "github.com/gin-contrib/sessions/gorm"
-	"github.com/gin-gonic/gin"
-	"gorm.io/driver/sqlite"
-	"gorm.io/gorm"
-)
-
-
 func main() {
-	// gin for web framework server
-	r := gin.Default()
-
-	// db
-	db, err := gorm.Open(sqlite.Open("./db/test.db"), &gorm.Config{})
-	if err != nil {
-		panic(err)
-	}
-	
-	// session store
-	sessionStore := gormsessions.NewStore(db, true, []byte("secret"))
-	r.Use(sessions.Sessions("mysession", sessionStore))
-
-	// routes
-	routes.SetupFrontendRoutes(r)
-
-	// webauthn config
-	webauthnController := webauthn.NewWebAuthnController()
-	webauthnRoutes := routes.NewWebAuthnRouteController(webauthnController)
-	webauthnRoutes.WebAuthnRoutes(r)
-
-	log.Println("Starting server on port 8080")
-	r.Run(":8080") // listen and serve on 0.0.0.0:8080
+	s := NewServer()
+	s.Start()
 }
---
+++ backend/server.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/baala3/passkey-demo/webauthn"
+	"github.com/gin-contrib/sessions"
+	gormsessions "github.com/gin-contrib/sessions/gorm"
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+type Server struct {
+	router *gin.Engine
+	webauthnController webauthn.WebAuthnController
+}
+
+func NewServer() *Server {
+	return &Server{
+		router: gin.Default(),
+		webauthnController: webauthn.NewWebAuthnController(),
+	}
+}
+
+func (s *Server) Start() {
+	db, err := gorm.Open(sqlite.Open("./db/test.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	//session store
+	sessionStore := gormsessions.NewStore(db, true, []byte("secret"))
+	s.router.Use(sessions.Sessions("mysession", sessionStore))
+
+	//routes
+	s.registerEndpoints()
+	log.Println("Starting server on port 8080")
+	_ = s.router.Run(":8080")
+}
+
+func (s *Server) registerEndpoints() {
+	s.router.Static("/static", "../frontend")
+	s.router.LoadHTMLGlob("../frontend/html/*")
+	s.router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "login.html", gin.H{})
+	})
+	s.router.GET("/home", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "home.html", gin.H{})
+	})
+
+	s.router.GET("/register/begin/:username", s.webauthnController.BeginRegistration)
+	s.router.POST("/register/finish/:username", s.webauthnController.FinishRegistration)
+	s.router.GET("/login/begin/:username", s.webauthnController.BeginLogin)
+	s.router.POST("/login/finish/:username", s.webauthnController.FinishLogin)
+}
