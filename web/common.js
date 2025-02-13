2b1e118cfaa3291917fec8be2997a884ec954514
mfw-bala <kamandla.b@moneyforward.co.jp>
2025-02-13 15:52:13 +0900

    use simplewebauthn client library

--- web/common.js
+++ web/common.js
@@ -7,21 +7,6 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 });
 
-// decode the base64url encoded value to a Uint8Array
-function bufferDecode(value) {
-  // Convert base64url to base64 by replacing "-" with "+" and "_" with "/"
-  value = value.replace(/-/g, "+").replace(/_/g, "/");
-
-  return Uint8Array.from(atob(value), (c) => c.charCodeAt(0));
-}
-
-function bufferEncode(value) {
-  return btoa(String.fromCharCode.apply(null, new Uint8Array(value)))
-    .replace(/\+/g, "-")
-    .replace(/\//g, "_")
-    .replace(/=+$/, "");
-}
-
 function showNotification(message) {
   document.getElementById("notification").innerHTML = message;
   setTimeout(() => {
