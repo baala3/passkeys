19bb4d64418a8d7f55ab1946b7eeec45af2d52ee
mfw-bala <kamandla.b@moneyforward.co.jp>
2025-02-20 14:43:04 +0900

    [frontend] introduce passkey management page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Login from "./pages/login";
-import Register from "./pages/register";
-import Homepage from "./pages/homepage";
+import Login from "./pages/login.tsx";
+import Register from "./pages/register.tsx";
+import Homepage from "./pages/homepage.tsx";
+import ManagePasskeys from "./pages/ManagePasskeys.tsx";
+
 function App(): React.ReactElement {
   return (
     <Router>
@@ -10,6 +12,7 @@ function App(): React.ReactElement {
         <Route path="/" element={<Login />} />
         <Route path="/sign-up" element={<Register />} />
         <Route path="/home" element={<Homepage />} />
+        <Route path="/passkeys" element={<ManagePasskeys />} />
       </Routes>
     </Router>
   );
--- client/src/pages/homepage.tsx
+++ client/src/pages/homepage.tsx
@@ -1,15 +1,15 @@
 import React, { useEffect } from "react";
-import { Layout } from "../components/layout/Layout";
+import { Layout } from "../components/layout/Layout.tsx";
 import { MenuItem } from "../components/navigation/MenuItem.tsx";
 import { Button } from "../components/input/Button.tsx";
 import { useNavigate } from "react-router-dom";
-import { isAuthenticated, logout } from "../utils/shared";
+import { isAuthenticated, logout } from "../utils/shared.ts";
 
 const MenuItems = [
   { title: "Change email address", link: "#" },
   { title: "Change password", link: "#" },
   { title: "Set up Two-Step Authentication", link: "#" },
-  { title: "Manage Passkeys", link: "#" },
+  { title: "Manage Passkeys", link: "/passkeys" },
   { title: "Delete Account", link: "#" },
 ];
 
